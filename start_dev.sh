c4aee2dc312c69fcbee46f1b32ccaf88e998ebe9
root <edward>
2014-07-20 19:35:01 -0700

    add start_dev.sh and with some usage comment in script

--- start_dev.sh
+++ start_dev.sh
@@ -1,2 +1,18 @@
 #! /usr/bin/env sh
-SQOR_RUN_SCRIPT_TYPE="python" sqor_deploy_env="local" erl -pa ebin deps/*/ebin deps/*/deps/*/ebin -sname script_cluster -s scripty_cluster
+echo on
+set -e
+export RUN_SCRIPT_TYPE="python"
+export DEDPLOY_ENV="local"
+if [[ -z "$RUN_SCRIPT_TYPE" ]]; then
+	echo "Make sure you set \$RUN_SCRIPT_TYPE before running this script"
+	echo "e.g. export RUN_SCRIPT_TYPE=\"python\""
+	exit 1
+fi
+
+if [[ -z "$DEPLOY_ENV" ]]; then
+	echo "Make sure you set \$DEPLOY_ENV before running this script"
+	echo "e.g. export DEPLOY_ENV=\"local\""
+	exit 1
+fi
+
+erl -pa ebin deps/*/ebin deps/*/deps/*/ebin -sname erl_script_cluster -s erl_script_cluster
